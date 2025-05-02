993cbc6bb74081c65b1979319a1922dad1ecf459
bernardlee <34589202+Bernard-LTW@users.noreply.github.com>
2025-05-02 15:22:55 +0100

    Update Firebase configuration: move sensitive keys to environment variables in .env.example and firebaseConfig.ts for improved security and flexibility.

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDsm6BC3QzeQ5bCou2l474xO-CfedQsfFY",
-  authDomain: "trip-tracker-cac70.firebaseapp.com",
-  projectId: "trip-tracker-cac70",
-  storageBucket: "trip-tracker-cac70.firebasestorage.app",
-  messagingSenderId: "995996851223",
-  appId: "1:995996851223:web:f258940e6dcecd1e849052"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
