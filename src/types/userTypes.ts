abd697309e42c4eb3cd7801d1fe6d76f5dff6196
bernardlee <34589202+Bernard-LTW@users.noreply.github.com>
2025-05-05 20:08:23 +0100

    Remove unused visaType property from UserPRInfo interface in userTypes.ts

--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -3,7 +3,6 @@ import { Timestamp } from 'firebase/firestore';
 export interface UserPRInfo {
   visaApprovalDate: string; //ISO date string format (YYYY-MM-DD)
   firstEntryToUK: string; //ISO date string format (YYYY-MM-DD)
-  visaType: string;
 }
 
 export interface User {
